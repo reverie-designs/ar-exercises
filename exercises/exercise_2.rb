dc5249275efed283cb5a47a7dce9601b53c13551
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-12-15 22:33:23 +0000

    read info of two store, update name of one store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,15 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+# Load the second store from the database and assign it to @store2.
+# Update the first store (@store1) instance in the database. (Change its name or something.)
 # Your code goes here ...
+
+@store = Store.find_by(id: 1);
+# puts @store.name
+@store2 = Store.find_by(id: 2);
+# puts @store2.name
+@store.update(name: "Wonderland")
+
+puts @store.name
